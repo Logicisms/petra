c691383ea98c412f527ae4d8250a3c71d8c50331
Logicisms <jeanluc.harbin@gmail.com>
2018-10-23 15:58:17 -0500

    adjustments to source directory and sasslintrc

--- config/grunt.js
+++ config/grunt.js
@@ -1,6 +1,6 @@
 module.exports = {
     dir: {
-        src: 'dev',
+        src: 'src',
         bld: 'build',
         out: 'dist'
     },
---
+++ src/README.md
@@ -0,0 +1,1 @@
+This is the source directory for Petra's build process. Framework source files are located in the 'petra' directory.
